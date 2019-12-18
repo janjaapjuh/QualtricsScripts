c2f06ffcc0fc4bc3c597f638eb517d54eec05c61
Janjaap Ree <janjaapree@gmail.com>
2019-12-18 13:17:08 +0100

    Update Qualtrics question hide plugin v0.2.js

--- Qualtrics question hide plugin v0.2.js
+++ Qualtrics question hide plugin v0.2.js
@@ -1,32 +1,19 @@
 Qualtrics.SurveyEngine.addOnload(function()
 {
-	/*
-	Log every click on an option in embedded data field "OptionsSelected".
-	Note: only visible in the raw export of responses.	
-
-	Steps to configure:
-	1. Add in survey flow an embedded data label called "OptionsSelected underneath your intro".
-	2. Set autonumbering under tools to use internal ID's for questions.
-	3. Add this script to MPC questions.
-	
-	Proudly developed by Janjaap Ree - V0.2
-	*/
+	/*Overwrite show method with original method + hide container after X ms*/
+	/*Proudly developed by Janjaap Ree - V0.2*/
 	var self = this;
-	this.questionclick = function(event,element){
-        // By default you get the click event as the first parameter and the clicked element as the second parameter
-        if (element.type == 'radio' || element.type == 'checkbox')
-        {
-            	var choiceNum = element.id.split('~')[2];
-		var choiceInfo = self.question.runtime.Choices[choiceNum];
-		if (element.type == 'radio' || choiceInfo.Selected){
-			var embeddedData = Qualtrics.SurveyEngine.getEmbeddedData("OptionsSelected");
-			if (embeddedData != null) {
-				Qualtrics.SurveyEngine.setEmbeddedData("OptionsSelected", embeddedData + ", " + "[" + self.questionId + "]" + choiceInfo.Display);
-			}
-			else {
-				Qualtrics.SurveyEngine.setEmbeddedData("OptionsSelected", "[" + self.questionId + "]" + choiceInfo.Display);
-			}
+	self.oldShow = this.question.show;
+	
+	self.hideContainer = function (){
+		if (self.questionContainer) {
+		    self.questionContainer.classList.add("hidden");
+		    self.question.runtime.SeparatorDisplayed = false;
 		}
-        }
-    }
+	}
+	
+	this.question.show = function (){
+		self.oldShow();
+		setTimeout(self.hideContainer, 5000); // 5 sec
+	}
 });
